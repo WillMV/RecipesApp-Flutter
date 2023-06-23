ccf7df3b5e39f4bc72a9047c19d0d85e9e3dd636
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-23 13:05:25 -0300

    renamed card gallery -> card middle

--- lib/components/card_middle.dart
+++ lib/components/card_middle.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CardGallery extends StatelessWidget {
-  const CardGallery({super.key, required this.srcImg, required this.name});
+class CardMiddle extends StatelessWidget {
+  const CardMiddle({super.key, required this.srcImg, required this.name});
   final String srcImg;
   final String name;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      semanticContainer: false,
-      color: Colors.grey,
+      // color: Colors.white60,
+      elevation: 5,
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         splashColor: Colors.blue,
@@ -17,7 +17,6 @@ class CardGallery extends StatelessWidget {
           debugPrint('Card tapped');
         },
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             Image.network(srcImg),
             Padding(
@@ -25,9 +24,12 @@ class CardGallery extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(
-                    name,
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  Flexible(
+                    child: Title(color: Colors.black, child: Text(
+                      name,
+                      style: const TextStyle(
+                          fontSize: 20),
+                    ),)
                   ),
                 ],
               ),
