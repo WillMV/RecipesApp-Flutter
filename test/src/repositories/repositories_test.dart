b23a2188b5ea45ace1bd9e0eb9fefb104a9a7dcf
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-27 12:42:22 -0300

    remove unnecessari imports

--- test/src/repositories/repositories_test.dart
+++ test/src/repositories/repositories_test.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
