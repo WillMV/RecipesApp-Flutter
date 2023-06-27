40730eb41feb66fbceeccbc8f39cbc93bf5dcb33
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-27 12:38:43 -0300

    refactor Pages drink, meal

--- lib/src/pages/drinks_page.dart
+++ lib/src/pages/drinks_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
-import '../../mock.dart';
+import '../models/drink_model.dart';
 import '../components/grid_gallery.dart';
+import '../repositories/recipes_repositories.dart';
 
-class DrinkPage extends StatelessWidget {
+class DrinkPage extends StatefulWidget {
   const DrinkPage({super.key});
 
   @override
+  State<DrinkPage> createState() => _DrinkPageState();
+}
+
+class _DrinkPageState extends State<DrinkPage> {
+  late Future<List<Drink>> drinks;
+
+  @override
+  void initState() {
+    super.initState();
+    drinks = FetchRecipes(null).fetchDrinks();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: GridGallery(recipes: DRINKS));
+    return FutureBuilder(
+      future: drinks,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GridGallery(recipes: snapshot.data as List<Drink>);
+        }
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
--- lib/src/pages/meals_page.dart
+++ lib/src/pages/meals_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
-import '../../mock.dart';
+import 'package:recipes_app/src/repositories/recipes_repositories.dart';
 import '../components/grid_gallery.dart';
+import '../models/meal_model.dart';
 
-class MealPage extends StatelessWidget {
+class MealPage extends StatefulWidget {
   const MealPage({super.key});
 
   @override
+  State<MealPage> createState() => _MealPageState();
+}
+
+class _MealPageState extends State<MealPage> {
+  late Future<List<Meal>> meals;
+
+  @override
+  void initState() {
+    super.initState();
+    meals = FetchRecipes(null).fetchMeals();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: GridGallery(recipes: MEALS));
+    return FutureBuilder(
+        future: meals,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return GridGallery(recipes: snapshot.data as List);
+          }
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return const CircularProgressIndicator();
+        });
   }
 }
