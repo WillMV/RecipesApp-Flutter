bc250abee8ba949da684f9b26bb57fde930015d5
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-27 16:39:23 -0300

    implement drinkController in drinkPage

--- lib/src/pages/drinks_page.dart
+++ lib/src/pages/drinks_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/src/controllers/constaints.dart';
+import 'package:recipes_app/src/controllers/drink_page_controller.dart';
 import '../models/drink_model.dart';
 import '../components/grid_gallery.dart';
-import '../repositories/recipes_repositories.dart';
 
 class DrinkPage extends StatefulWidget {
   const DrinkPage({super.key});
@@ -12,26 +13,53 @@ class DrinkPage extends StatefulWidget {
 
 class _DrinkPageState extends State<DrinkPage> {
   late Future<List<Drink>> drinks;
+  final controller = DrinkPageController(null);
+
+  Widget _start() {
+    return Container();
+  }
+
+  Widget _loading() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _error() {
+    return const Text('Falhou ;-;');
+  }
+
+  Widget _success() {
+    return GridGallery(recipes: controller.drinks);
+  }
+
+  Widget stateManagement(Status status) {
+    switch (status) {
+      case Status.start:
+        return _start();
+      case Status.loading:
+        return _loading();
+      case Status.error:
+        return _error();
+      case Status.success:
+        return _success();
+      default:
+        return _start();
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    drinks = FetchRecipes(null).fetchDrinks();
+    controller.start();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: drinks,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return GridGallery(recipes: snapshot.data as List<Drink>);
-        }
-        if (snapshot.hasError) {
-          return Text(snapshot.error.toString());
-        }
-        return const CircularProgressIndicator();
-      },
-    );
+    return AnimatedBuilder(
+        animation: controller.status,
+        builder: (ctx, child) {
+          return stateManagement(controller.status.value);
+        });
   }
 }
