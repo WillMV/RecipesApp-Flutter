458e2a010219073f053183149d056c3ddedc2f42
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-28 18:28:23 -0300

    create routes  in main with home and detail pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-// ignore_for_file: unused_import
 import 'package:flutter/material.dart';
-import 'package:recipes_app/src/components/card_bigger.dart';
-import 'package:recipes_app/src/components/card_middle.dart';
-import 'package:recipes_app/src/pages/drinks_page.dart';
-import 'package:recipes_app/src/pages/fav_page.dart';
-import 'package:recipes_app/src/pages/meals_page.dart';
 import 'package:recipes_app/src/controllers/theme_controller.dart';
-// import '../test/src/repositories/mock.dart';
+import 'package:recipes_app/src/pages/drink_details.dart';
+import 'package:recipes_app/src/pages/meals_details.dart';
+import 'src/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,53 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
+      animation: ThemeProvider.instance,
       builder: (context, child) {
         return MaterialApp(
           theme: ThemeProvider.instance.getTheme(),
           title: 'Recipes App',
-          home: const HomePage(),
+          routes: {
+            '/home': (context) => const HomePage(),
+            '/detail/drink':(context) => const DrinkDetailPage(),
+            '/detail/meal':(context) => const MealDetailPage(),
+          },
+          initialRoute: '/home',
         );
       },
-      animation: ThemeProvider.instance,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // const srcImg = 'https://a.cdn-hotels.com/gdcs/production42/d1958/7cc955d5-867f-4f7f-980b-3f8c95c3933f.jpg?impolicy=fcrop&w=800&h=533&q=medium';
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-                bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.local_dining)),
-                Tab(icon: Icon(Icons.liquor)),
-                Tab(
-                  icon: Icon(Icons.favorite),
-                )
-              ],
-            )),
-            body: const TabBarView(children: [
-              MealPage(),
-              DrinkPage(),
-              DrinkPage(),
-              // FavPage(),
-            ]),
-            floatingActionButton: AnimatedBuilder(
-              animation: ThemeProvider.instance,
-              builder: (ctx, child) {
-                return FloatingActionButton(
-                  child: ThemeProvider.instance.getButtonIcon(),
-                  onPressed: () {
-                    ThemeProvider.instance.changeTheme();
-                  },
-                );
-              },
-            )));
-  }
-}
---
+++ lib/src/controllers/detail_page_controller.dart
@@ -0,0 +1,9 @@
+import '../models/drink_model.dart';
+import '../models/meal_model.dart';
+
+class DetailPageController {
+  static DetailPageController instance = DetailPageController();
+
+  Drink? drink;
+  Meal? meal;
+}
