57335fef809b8b552b97d602ab3bdb0e2ca7bf56
Willian M. Vargas <willianm.vargas@gmail.com>
2023-06-30 16:18:58 -0300

    update repositorie to receive ingredients

--- lib/src/repositories/recipes_repositories.dart
+++ lib/src/repositories/recipes_repositories.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:ffi';
+
 import 'package:dio/dio.dart';
 import 'package:recipes_app/src/models/drink_model.dart';
 import 'package:recipes_app/src/models/meal_model.dart';
@@ -16,13 +18,31 @@ class FetchRecipes {
     if (statusCode != 200) throw Exception('Requisição falhou $statusCode.');
   }
 
-   Future<List<Drink>> fetchDrinks() async {
+  Map<String, dynamic> getterIngredients(Map<String, dynamic> json) {
+    List<String> ingredients = [];
+    int i = 1;
+    String key = 'strIngredient$i';
+
+    while (json['strIngredient$i'] != null && json['strIngredient$i'] != "") {
+      ingredients.add(json[key]);
+      i++;
+      key = 'strIngredient$i';
+    }
+
+    json["listIngredients"] = ingredients;
+    return json;
+  }
+
+  Future<List<Drink>> fetchDrinks() async {
     final response = await _client.get(drinkUrl);
 
     resValidate(response.statusCode);
 
     final jsonList = response.data["drinks"] as List;
-    return jsonList.map((json) => Drink.fromJson(json)).toList();
+    return jsonList.map((json) {
+      Map<String, dynamic> jsonWithIngredientsList = getterIngredients(json);
+      return Drink.fromJson(jsonWithIngredientsList);
+    }).toList();
   }
 
   Future<List<Meal>> fetchMeals() async {
@@ -31,6 +51,9 @@ class FetchRecipes {
     resValidate(response.statusCode);
 
     final jsonList = response.data["meals"] as List;
-    return jsonList.map((json) => Meal.fromJson(json)).toList();
+    return jsonList.map((json) {
+      Map<String, dynamic> jsonWithIngredientsList = getterIngredients(json);
+      return Meal.fromJson(jsonWithIngredientsList);
+    }).toList();
   }
 }
