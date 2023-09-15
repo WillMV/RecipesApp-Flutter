7dbe14bfb5f88e36b7af45c1675f1d02e90ff6fe
Willian M Vargas <willianm.vargas@gmail.com>
2023-09-14 22:04:52 -0300

    restylezed cardsmall

--- lib/src/components/cards/card_small.dart
+++ lib/src/components/cards/card_small.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/src/components/buttons/fav_button.dart';
 import 'package:recipes_app/src/controllers/detail_page_controller.dart';
+import 'package:recipes_app/src/controllers/fav_controller.dart';
 import 'package:recipes_app/src/models/drink_model.dart';
 import 'package:recipes_app/src/models/meal_model.dart';
 import 'package:recipes_app/src/models/recipe_model.dart';
 
-class CardSmall extends StatelessWidget {
-  CardSmall({super.key, required this.recipe});
+class CardSmall extends StatefulWidget {
+  const CardSmall({super.key, required this.recipe});
   final Recipe recipe;
+
+  @override
+  State<CardSmall> createState() => _CardSmallState();
+}
+
+class _CardSmallState extends State<CardSmall> {
   final detailController = DetailPageController.instance;
+  late FavController favController;
+
+  @override
+  void initState() {
+    super.initState();
+    favController = FavController(widget.recipe);
+    favController.isFavVerify();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,34 +33,36 @@ class CardSmall extends StatelessWidget {
       child: InkWell(
         splashColor: Colors.blue,
         onTap: () {
-          if (recipe.toString() == "Instance of 'Drink'") {
-            detailController.drink = recipe as Drink;
+          if (widget.recipe.toString() == "Instance of 'Drink'") {
+            detailController.drink = widget.recipe as Drink;
             Navigator.pushNamed(context, '/detail/drink');
           } else {
-            detailController.meal = recipe as Meal;
+            detailController.meal = widget.recipe as Meal;
             Navigator.pushNamed(context, '/detail/meal');
           }
         },
-        child: Column(
+        child: Stack(
           children: [
-            Image.network(recipe.thumb,
-                height: 130, width: 200, fit: BoxFit.cover),
-            Padding(
-              padding: const EdgeInsets.only(left: 8, top: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Flexible(
-                      child: Title(
-                    color: Colors.black,
-                    child: Text(
-                      recipe.name,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  )),
-                ],
+            Image.network(
+              widget.recipe.thumb,
+              // fit: BoxFit.contain,
+            ),
+            Center(
+              child: Text(
+                widget.recipe.name,
+                style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(blurRadius: 10),
+                      BoxShadow(
+                          blurRadius: 5,
+                          blurStyle: BlurStyle.outer,
+                          spreadRadius: 1)
+                    ]),
               ),
-            )
+            ),
+            FavButton(favController: favController)
           ],
         ),
       ),
